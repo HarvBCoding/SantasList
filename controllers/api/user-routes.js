d74446f8ae25c2e379238024e930e15bb239ee33
Lori Timmons <lorietimmons@gmail.com>
2021-11-10 23:18:52 -0600

    user routes get one

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   User.findAll({
     attributes: {
       exclude: ["password"],
@@ -12,6 +12,30 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+
+  router.get("/:id", (req, res) => {
+    User.findOne({
+      attributes: {
+        exclude: ["password"],
+      },
+      where: {
+        username: req.params.username,
+      },
+    })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({
+            message: "No user found with this username",
+          });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;
