d6fe156c820871eb50ffc257506a4e3558c25048
Lori Timmons <lorietimmons@gmail.com>
2021-11-10 20:50:03 -0600

    user routes started

--- models/User.js
+++ models/User.js
@@ -4,16 +4,11 @@ const sequelize = require("../config/connection");
 class User extends Model { }
 User.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            primaryKey: true,
         },
         email: {
             type: DataTypes.STRING,
