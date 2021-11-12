23fff3e4d82148a604a1b5ac8aeec254e7806c8f
Breania Harvey <harvey.breania@gmail.com>
2021-11-11 20:30:17 -0600

    id added back to user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// GET all users
 router.get("/", (req, res) => {
   User.findAll({
     attributes: {
@@ -12,30 +13,29 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
 
-  router.get("/:id", (req, res) => {
-    User.findOne({
-      attributes: {
-        exclude: ["password"],
-      },
-      where: {
-        username: req.params.username,
-      },
+// Get one user
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({
+          message: "No user found with this username",
+        });
+        return;
+      }
+      res.json(userData);
     })
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({
-            message: "No user found with this username",
-          });
-          return;
-        }
-        res.json(userData);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-        console.log(err);
-      });
-  });
+    .catch((err) => {
+      res.status(500).json(err);
+      console.log(err);
+    });
 });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -4,11 +4,16 @@ const sequelize = require("../config/connection");
 class User extends Model { }
 User.init(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
-            primaryKey: true,
+            unique: true
         },
         email: {
             type: DataTypes.STRING,
