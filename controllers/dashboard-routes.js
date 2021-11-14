7dd7f3aebc607fe8b8a5246dcd7166b250acaaa1
Breania Harvey <harvey.breania@gmail.com>
2021-11-13 23:31:23 -0600

    page is rendering/ data from routes will not render

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Recipient, Gift, User } = require('../models');
+
+// GET all recipients for dashboard
+router.get('/', (req, res) => {
+    Recipient.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['name', 'relationship'],
+        include: [
+            {
+                model: Gift,
+                attributes: ['gift_name', 'price']
+            }
+        ]
+    })
+    .then(recipientData => {
+        const recipients = recipientData.map(rec => rec.get({ plain: true}));
+        res.render('dashboard', { recipients, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// edit single post
+router.get('/edit/:id', (req, res) => {
+    Recipient.findByPk(req.params.id, {
+        attributes: ['name', 'relationship'],
+        include: [
+            {
+                model: Gift,
+                attributes: ['gift_name', 'price']
+            }
+        ]
+    })
+    .then(recipientData => {
+        if (recipientData) {
+            const rec = recipientData.get({ plain: true });
+            
+            res.render('edit-recipient', {rec, loggedIn: true});
+
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
