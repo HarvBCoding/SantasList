32fdab35a0d6581afcc9e191c2f344bc8c98ec50
Lori Timmons <lorietimmons@gmail.com>
2021-11-15 20:10:04 -0600

    edit-recipient HB

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,11 +2,13 @@ const { User } = require('../models');
 
 const userdata = [
     {
+        id: 1,
         username: 'malicious1',
         email: 'harveyb@gmail.com',
         password: 'password321'
     },
     {
+        id: 2,
         username: 'lorimucho',
         email: 'lorimucho@gmail.com',
         password: 'password321'
