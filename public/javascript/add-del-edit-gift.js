b42921c88c4513b763173a548fc27a43fe1e8c84
Lori Timmons <lorietimmons@gmail.com>
2021-11-15 23:44:18 -0600

    add gift frontend JS

---
+++ public/javascript/add-del-edit-gift.js
@@ -0,0 +1,31 @@
+async function addGiftHandler(event) {
+  event.preventDefault();
+
+  const gift_name = document.querySelector("#add-gift").value;
+  const price = document.querySelector("#add-price").value;
+ 
+  const recipient_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  
+  const response = await fetch ('/api/gifts', {
+      method: 'post',
+      body: JSON.stringify({
+          gift_name,
+          price,
+          recipient_id
+      }),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+      document.location.reload();
+  } else {
+      alert(response.statusText);
+  }
+}
+
+document.querySelector('.add-gift-form').addEventListener('submit' , addGiftHandler);
+
