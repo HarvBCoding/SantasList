126fcde687d194ff43be15db87d6b3103a3cb10e
Lori Timmons <lorietimmons@gmail.com>
2021-11-16 20:30:44 -0600

    edit gift list

--- public/javascript/add-del-edit-gift.js
+++ public/javascript/add-del-edit-gift.js
@@ -44,10 +44,49 @@ async function deleteGiftHandler(event) {
   }
 }
 
-document.querySelector('.add-gift-form').addEventListener('submit' , addGiftHandler);
+// edit gift 
+
+async function editGiftHandler(event) {
+    event.preventDefault();
+
+    const gift_name = document.querySelector("#gift-name").value;
+    console.log("🚀 ~ file: add-del-edit-gift.js ~ line 53 ~ editGiftHandler ~ #gift-name", #gift-name)
+
+    const price = document.querySelector("#gift-price").value;
+   
+    const gift_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+
+    const response = await fetch(`/api/gifts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            gift_name,
+            price,
+            gift_id
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      document.location.reload(`/dashboard/`);
+    } else {
+      alert(response.statusText);
+    }
+  }
+    
+    // end
+
+
+  document.querySelector('.edit-post-form').addEventListener('submit', editGiftHandler);
+  
+  document.querySelector('.add-gift-form').addEventListener('submit' , addGiftHandler);
+  
 
 const deleteBtns = document.querySelectorAll('.delete-gift-btn');
 for (let i = 0; i < deleteBtns.length; i++) {
   deleteBtns[i].addEventListener('click', deleteGiftHandler);
 }
+
 
