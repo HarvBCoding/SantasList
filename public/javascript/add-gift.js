8b0517e45eca8a2138546b44419900a76e2f36a8
Breania Harvey <harvey.breania@gmail.com>
2021-11-21 14:11:18 -0600

    changes to handlebars

--- public/javascript/add-gift.js
+++ public/javascript/add-gift.js
@@ -2,16 +2,12 @@ async function addGiftHandler(event) {
     event.preventDefault();
   
     const gift_name = document.querySelector("#add-gift").value;
-    console.log("🚀 ~ file: add-del-edit-gift.js ~ line 5 ~ addGiftHandler ~ gift_name", gift_name)
   
     const price = document.querySelector("#add-price").value;
-    console.log("🚀 ~ file: add-del-edit-gift.js ~ line 8 ~ addGiftHandler ~ price", price)
    
     const recipient_id = window.location.toString().split("/")[
       window.location.toString().split("/").length - 1
-    ];
-    console.log("🚀 ~ file: add-del-edit-gift.js ~ line 11 ~ addGiftHandler ~ recipient_id", recipient_id)
-  
+    ];  
   
     const response = await fetch ('/api/gifts', {
         method: 'post',
